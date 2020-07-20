83137e7bbb2e902fbc92d944952c58ccfa2001ed
Jim Kong <jckong99@gmail.com>
2020-07-20 19:38:34 -0400

    Added preparation for user login/logout features

--- backend/project/__init__.py
+++ backend/project/__init__.py
@@ -2,6 +2,7 @@
 
 from flask import Flask
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 import mysql.connector as mariadb
 from dotenv import load_dotenv
 import os
@@ -14,6 +15,10 @@ app = Flask(__name__, template_folder=os.path.abspath('../frontend'))
 # Apparently setting `SECRET_KEY` helps against XSS
 app.config['SECRET_KEY'] = os.getenv('Flask_SECRET_KEY')
 
+# Create instance of and initialize LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 # Create usable instance of encryptor
 bcrypt = Bcrypt(app)
 
@@ -23,6 +28,27 @@ dbcursor = dbconnection.cursor()
 
 
 #from project import routes
+
+
+''' USER CLASS (for use with Flask-Login) '''
+
+
+from flask_login import UserMixin
+
+
+class User(UserMixin):
+    def __init__(self, email, display_name, ukey, icon_url, credential_id, pub_key, sign_count, rp_id):
+        self.email = email                  # str
+        self.display_name = display_name    # str
+        self.ukey = ukey                    # str
+        self.icon_url = icon_url            # str
+        self.credential_id = credential_id  # str
+        self.pub_key = pub_key              # str
+        self.sign_count = sign_count        # int
+        self.rp_id = rp_id                  # str
+
+    def __repr__(self):
+        return 'User %s (Email: %s)' % (self.display_name, self.email)
 
 
 ''' UTILITY  '''
