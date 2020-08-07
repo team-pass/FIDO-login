7a0052e029377e9e9cdb99b69bfd880d47751ced
John Wu <johnwuuni@gmail.com>
2020-08-07 15:59:32 -0400

    Added CSRF protection from Flask

---
+++ FIDO-login/fido_app/__init__.py
@@ -0,0 +1,44 @@
+''' BACKEND PROJECT PACKAGE INITIALIZATION '''
+
+from flask import Flask
+from flask_bcrypt import Bcrypt
+import mysql.connector as mariadb
+from .utils import ensure_environ_vars
+from dotenv import load_dotenv
+import sys, time
+import os
+
+# Set the local environment based on a `backend/.env` file
+load_dotenv()
+
+# Throw a descriptive error if the user's environment variables are missing
+ensure_environ_vars(
+    ['FLASK_SECRET_KEY', 'DB_USER', 'DB_PASSWORD', 'DB_NAME', 'DB_HOST',]
+)
+
+# Create instance of Flask application
+app = Flask(__name__)
+
+# Apparently setting `SECRET_KEY` helps against XSS
+app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
+
+# Create usable instance of encryptor
+bcrypt = Bcrypt(app)
+
+# Establish database connection
+dbconnection = mariadb.connect(
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+    database=os.getenv('DB_NAME'),
+    host=os.getenv('DB_HOST'),
+)
+dbcursor = dbconnection.cursor()
+
+# import declared routes
+from . import routes
+
+#### DEBUGGING ####
+
+# Print message with timestamp to file (stderr by default)
+def log(msg, sep='#', file_out=sys.stderr):
+    print('%s\n(%s) %s\n%s' % (sep * 64, time.ctime(), msg, sep * 64), file=file_out)
