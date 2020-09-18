0bc41dfacd9f0eba98b2254ef1c914844e488a05
jonahtash <jtash@umd.edu>
2020-09-18 13:16:50 -0400

    added testing framework

---
+++ tests/test.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('..')
+from fido_app import app
+import flask
+
+def test():
+    with app.test_request_context('/?name=Peter'):
+        assert flask.request.path == '/'
+        assert flask.request.args['name'] == 'Peter'
