31f22ea82134a82523c436c1b85dfdca481b1429
Zach Breit <zbreit18@gmail.com>
2021-07-14 19:12:24 -0400

    Submit interactions when the form is submitted

--- fido_app/static/scripts/interaction-logging.js
+++ fido_app/static/scripts/interaction-logging.js
@@ -7,10 +7,11 @@
  * 
  * Otherwise, this script will listen for `focus` events.
  */
-const DEFAULT_EVENTS = ['focus'];
 const interactions = [];
 const timedElements = document.querySelectorAll("[timed-element]");
+const form = document.querySelector("#primary-form");
 timedElements.forEach(attachListeners);
+form.addEventListener('submit', submitInteractions);
 
 /**
  * Attaches timing listeners to the given HTML element. It will attach events to
@@ -42,9 +43,30 @@ function attachListeners(element) {
  * @returns list of event types to listen on
  */
 function getTimedEventsFor(element) {
+    const DEFAULT_EVENTS = ['focus'];
     const customEvents = element.getAttribute("timed-events");
 
     return (customEvents && customEvents.split(" ")) || DEFAULT_EVENTS;
 }
 
-// TODO: possibly add interactions on page refresh
+/**
+ * Submits all interactions that were logged on this page.
+ * @param {Event} event the form submit event
+ */
+async function submitInteractions(event) {
+    event.preventDefault();
+    const url = "/interactions/submit/";
+
+    try {
+        const response = await fetch(url, {
+            method: 'post',
+            body: JSON.stringify(interactions),
+        });
+
+        if (!response.ok) {
+            console.error("Posting the interactions didn't succeed. Response: ", response);
+        }
+    } catch(e) {
+        console.error("Couldn't submit:", e);
+    }
+}
