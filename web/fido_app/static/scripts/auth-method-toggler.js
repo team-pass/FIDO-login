af14f51c6b390bb9744e594244b9398d3d73a914
Zach Breit <zbreit18@gmail.com>
2022-01-15 11:17:01 -0500

    Implemented auth method preference saving

--- web/fido_app/static/scripts/auth-method-toggler.js
+++ web/fido_app/static/scripts/auth-method-toggler.js
@@ -1,3 +1,7 @@
+const AUTH_METHOD = 'authentication-method';
+const FIDO = 'fido';
+const PASSWORDS = 'passwords';
+
 const methodToggler = document.getElementById("auth-method-toggler");
 const passwordInputs = document.querySelectorAll("#password-inputs input");
 
@@ -31,4 +35,82 @@ function setRequiredFields() {
     }
 }
 
+/**
+ * Saves the user's preferred authentication method to `window.localStorage`
+ */
+function saveAuthenticationPreference() {
+    const authPreference = methodToggler.getAttribute("aria-expanded") === "true"
+        ? FIDO
+        : PASSWORDS;
+
+    if (storageAvailable('localStorage')) {
+        localStorage.setItem(AUTH_METHOD, authPreference);
+        console.log("Setting auth preference to", localStorage.getItem(AUTH_METHOD));
+    } else {
+        console.warn("Unable to save authentication preference to local storage");
+    }
+}
+
+/**
+ * Sets the status of the authentication method toggle based on the user's
+ * last-used sign in/registration method.
+ */
+function loadAuthenticationPreference() {
+    if (!storageAvailable('localStorage')) {
+        console.warn("Unable to load authentication preference from local storage");
+        return;
+    }
+
+    const authMethod = localStorage.getItem(AUTH_METHOD);
+
+    switch(authMethod) {
+        case FIDO:
+            methodToggler.click();
+            break;
+
+        case PASSWORDS:
+            // Already in the default state
+            break;
+
+        default:
+            console.error(`Invalid authentication preference ${authMethod}`);
+    }
+
+    console.log("Loaded preferred authentication method:", authMethod);
+}
+
+/**
+ * Detects whether the browser supports the particular storage type. Taken from
+ * https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#testing_for_availability
+ */
+function storageAvailable(type) {
+    var storage;
+    try {
+        storage = window[type];
+        var x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
+
+
 methodToggler.addEventListener("click", setRequiredFields);
+methodToggler.addEventListener("click", saveAuthenticationPreference);
+
+// Load authentication preference once when this script loads
+loadAuthenticationPreference();
